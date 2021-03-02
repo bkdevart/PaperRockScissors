0501944d65a4afef39144b2c2d090c65ab07e11d
Brandon Knox <bknox.dev@gmail.com>
2021-03-02 14:25:46 -0700

    finished logic and implementation

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,44 +10,70 @@ import SwiftUI
 
 
 struct ContentView: View {
-    //    Start with a Single View App template, then create a property to store the three possible moves: ["Rock", "Paper", "Scissors"].
-    let allMoves = ["Rock", "Paper", "Scissors"]
+
+    @State private var allMoves = ["Rock", "Paper", "Scissors"]
+    @State private var currentChoice = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var userScore = 0
     
-//    You’ll need to create two @State properties to store the app’s current choice and whether the player should win or lose.
-    @State var currentChoice = ""
-    @State var winOrLose = true
-    @State var userScore = 0
-    
-//    You can use Int.random(in:) to select a random move. You can use it for whether the player should win too if you want, but there’s an even easier choice: Bool.random() is randomly true or false.
-    var shouldWin: String {
-        winOrLose = Bool.random()
-        if winOrLose {
+    var body: some View {
+        VStack {
+            Text("Your score: \(userScore)")
+            Text(allMoves[currentChoice])
+            Text(winOrLose())
+            Spacer()
+            ForEach(0 ..< 3) { number in
+                Button(allMoves[number],
+                       action: { userCheck(number) })
+            }
+        }
+    }
+
+    func winOrLose() -> String {
+        if shouldWin {
             return "Try to win!"
         } else {
             return "Try to lose!"
         }
     }
     
-    var body: some View {
-//        Create a VStack showing the player’s score, the app’s move, and whether the player should win or lose. You can use if shouldWin to return one of two different text views.
-        VStack {
-            Text("Your score: \(userScore)")
-            Text(currentChoice)
-            Text(shouldWin)
-//            The important part is making three buttons that respond to the player’s move: Rock, Paper, or Scissors.
-            Spacer()
-            ForEach(0 ..< allMoves.count) {
-                Button(allMoves[$0]) {
-                    appChoice()
-                }
+    func userCheck(_ number: Int) {
+        // get user choice
+        let userChoice = allMoves[number]
+        let challengeChoice = allMoves[currentChoice]
+        var win = false
+        switch (challengeChoice) {
+        case "Rock":
+            if userChoice == "Paper" {
+                win = true
+            } else {
+                win = false
             }
+        case "Paper":
+            if userChoice == "Scissors" {
+                win = true
+            } else {
+                win = false
+            }
+        case "Scissors":
+            if userChoice == "Rock" {
+                win = true
+            } else {
+                win = false
+            }
+        default:
+            win = false
         }
+        if shouldWin && win {
+            userScore += 1
+        } else if (shouldWin == false && win == false) {
+            userScore += 1
+        } else {
+            userScore -= 1
+        }
+        currentChoice = Int.random(in: 0...2)
+        shouldWin = Bool.random()
     }
-    
-    func appChoice() {
-        currentChoice = allMoves[Int.random(in: 0...2)]
-    }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
