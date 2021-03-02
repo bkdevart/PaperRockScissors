ca4d6de5d5c1bf664a00b345e3dba9c54cf3247f
Brandon Knox <bknox.dev@gmail.com>
2021-03-01 20:07:25 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Brandon Knox on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
